4821e21a03afecda84bff4b6b82a2bcb54d4d3fe
Arun1102 <78301915+Arun1102@users.noreply.github.com>
2022-01-31 23:22:32 +0800

    second changes

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -8,14 +8,12 @@ function Navbar() {
           <ul className='Navbar-row'>
               
               <img className='Navbar-img' src="https://www.adobe.com/express/create/media_127a4cd0c28c2753638768caf8967503d38d01e4c.jpeg?width=400&format=jpeg&optimize=medium"></img>
-              
-         
-           
+            
                 |
             
-              <li className='Navbar-list'>Home</li>
-              <li className='Navbar-list'>Login</li>
-              <button className='Navbar-button'>Sign Up</button>
+            <li className='Navbar-list'>Home</li>
+            <li className='Navbar-list'> Login</li>
+            <button className='Navbar-button'>Sign Up</button>
               
           </ul>
       </nav>
