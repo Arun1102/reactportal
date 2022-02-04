b904af0a74e2ff69e0baf7187cd80800eb84c37e
Arun1102 <78301915+Arun1102@users.noreply.github.com>
2022-02-04 20:51:29 +0800

    save leaveapply & signin

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Navbar from './Components/navbar/Navbar'
 import Home from './Components/Home/Home'
 import LeaveList from './Components/LeaveList/LeaveList';
 import ApplyLeave from './Components/ApplyLeave/ApplyLeave';
+import Signin from './Components/SignIn/signin';
 function App() {
   return (
     <div className="App">
       <Navbar/>
       {/* <Home/> */}
       {/* <LeaveList/> */}
-      <ApplyLeave/>
+      {/* <ApplyLeave/> */}
+      <Signin/>
     </div>
   );
 }
--- src/Components/ApplyLeave/ApplyLeave.js
+++ src/Components/ApplyLeave/ApplyLeave.js
@@ -6,6 +6,7 @@ function ApplyLeave() {
 
 
   const [selectedDate, setSelectedDate] = useState();
+  const [selectedEndDate, setSelectedEndDate] = useState();
   const [formData,setFormData] = useState({
     LeaveType:""
   });
@@ -13,18 +14,21 @@ function ApplyLeave() {
   return <div className='ApplyLeave'>
       <div className='LeaveInfo'>
       <span className='Leavetitle'>Apply for leave</span>
+      </div>
+      <div className='LeaveInfo2'> 
       <h6>You have 6 leave in your account for 2022</h6>
       </div>
      <form>
        <div className='ApplyLeave-Radio'>
-         <span>Leave Type</span><br/>
+         <span >Leave Type</span><br/>
          <label>Annual Leave</label>
-         <input type="radio" name="LeaveType" value="Annual Leave"></input>
+         <input className='radiobutton1' type="radio" name="LeaveType" value="Annual Leave"></input>
          <label>Medical Leave</label>
-         <input type="radio" name="LeaveType" value="Medical Leave"></input>
+         <input className='radiobutton2'  type="radio" name="LeaveType" value="Medical Leave"></input>
        </div>
      </form>
     <div className='ApplyLeave-Date'>
+      <span >Start date</span>
     <DatePicker 
     selected={selectedDate} 
     placeholderText='From' 
@@ -32,18 +36,22 @@ function ApplyLeave() {
     minDate={new Date()} 
     onChange={(date)=>setSelectedDate(date) }
     filterDate = {(date)=> date.getDay() != 6 && date.getDay() != 0 }
+    wrapperClassName="datePicker1"
     />
+    <span >End date</span>
     <DatePicker 
-    selected={selectedDate} 
+    selected={selectedEndDate} 
     placeholderText='To' 
     dateFormat='dd/MM/yyyy' 
     minDate={new Date()} 
-    onChange={(date)=>setSelectedDate(date) }
+    onChange={(date)=>setSelectedEndDate(date) }
     filterDate = {(date)=> date.getDay() != 6 && date.getDay() != 0 }
+    wrapperClassName="datePicker2"
     />
 
     </div>
      
+     <div className='Deparment-choose'>
     <label for="cars">Choose Department  </label>
 
     <select name="Department" id="Department">
@@ -52,17 +60,19 @@ function ApplyLeave() {
       <option value="Administration">Administration</option>
       <option value="HR/Manager">HR/Manager</option>
     </select>
-
+    </div>
         
-    <h1>REASON</h1>
 
+<div className='Reason-apply'>
+
+<h1>REASON:</h1>
 <form action="/action_page.php">
-  <label for="fname" >Reasons  </label>
-  <input type="text" placeholder='Optional' id="fname" name="fname"/><br/>
   
+  
+  <textarea className='ReasonHold' type="text" placeholder='Type here leave reason' id="fname" name="fname"></textarea><br/>
 </form>
-
-   <button>Submit</button>
+</div>
+   <button className='ApplyButton'>Submit</button>
 
   </div>;
   
